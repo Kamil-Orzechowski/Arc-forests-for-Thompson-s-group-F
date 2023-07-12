951ad3ae71f78435dc7e34fe668e83275f4a2fac
Kamil Orzechowski <kam.kr.orzechowski@gmail.com>
2023-07-12 13:36:07 +0200

    Plotting improved

--- arc_plot.py
+++ arc_plot.py
@@ -6,7 +6,7 @@ def semicircle_path(left_end, right_end, N=100):
     center = (left_end + right_end) / 2
     radius = (right_end - left_end) / 2
     x = center + radius * np.cos(t)
-    y = np.sin(t)
+    y = radius * np.sin(t)
     path = f'M {x[0]}, {y[0]}'
     for k in range(1, len(t)):
         path += f'L{x[k]}, {y[k]}'
--- diagram_plot.py
+++ diagram_plot.py
@@ -9,7 +9,18 @@ def draw_diagram(word: str):
     intervals = diagram.get_intervals()
     arcs = [dict(type="path",
                  path=semicircle_path(interval[0], interval[1])) for interval in intervals]
+    x_ticks = [intervals[0][0]] + [interval[1] for interval in intervals]
     fig = go.Figure()
     fig.update_layout(shapes=arcs)
+    fig.update_layout(xaxis= {
+                        'tickmode': 'array',
+                        'tickvals': x_ticks,
+                        'ticktext': x_ticks
+                    },
+                      yaxis={
+                        'scaleanchor': 'x',
+                        'scaleratio': 1,
+                        'showticklabels': False
+                    })
     fig.add_trace(go.Scatter(x=[basepoint], y=[0], mode='markers'))
     return fig

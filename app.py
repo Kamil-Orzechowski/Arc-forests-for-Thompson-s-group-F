3f2ecf6c262543c83a4c7ad612db04d8591af5a6
Kamil Orzechowski <kam.kr.orzechowski@gmail.com>
2023-07-16 10:10:45 +0200

    Displaying numbers as dyadic fractions enabled

--- app.py
+++ app.py
@@ -10,10 +10,15 @@ app.layout = html.Div([
     dcc.Input(id='word', type='text', pattern=r'[aAbB]*', value=''),
     html.Div(children='Specify the number of trivial arcs appended to each end of the essential part of the diagram'),
     dcc.Input(id='tail_length', type='number', min=0, value=100),
-    html.Div(children='Specify how far from the center the initial range on the x axis should be'),
+    html.Div(
+        children='Specify how far from the center the initial range on the x axis should be'),
     dcc.Input(id='range_around_center', type='number', min=0, value=5),
     html.Div(children='Center the visible part of the diagram at'),
-    dcc.RadioItems(id='center', options=['zero', 'the basepoint'], value='zero'),
+    dcc.RadioItems(id='center', options=[
+                   'zero', 'the basepoint'], value='zero'),
+    html.Div(children='Select number format'),
+    dcc.RadioItems(id='format', options=[
+                   'decimal', 'dyadic fraction'], value='decimal'),
     dcc.Graph(id='diagram', figure={})
 ])
 
@@ -23,10 +28,11 @@ app.layout = html.Div([
     Input('word', 'value'),
     Input('tail_length', 'value'),
     Input('range_around_center', 'value'),
-    Input('center', 'value')
+    Input('center', 'value'),
+    Input('format', 'value')
 )
-def update_diagram(input_word, tail_length, range_around_center, center):
-    return draw_diagram(input_word, tail_length, range_around_center, center)
+def update_diagram(input_word, tail_length, range_around_center, center, format):
+    return draw_diagram(input_word, tail_length, range_around_center, center, format)
 
 
 if __name__ == '__main__':
--- diagram_plot.py
+++ diagram_plot.py
@@ -1,13 +1,15 @@
 import plotly.graph_objects as go
 from arc_plot import semicircle_path
+from fractions import Fraction
 from model import trivial_diagram
 
 
-def draw_diagram(word: str, trivial_tail_length=100, range_around_center=5, center='zero'):
+def draw_diagram(word: str, trivial_tail_length=100, range_around_center=5, center='zero', format='decimal'):
     diagram = trivial_diagram.apply_word(word)
     basepoint = diagram.basepoint
     intervals = diagram.get_intervals()
     maximal_intervals = diagram.get_intervals(maximal_only=True)
+
     left_end, right_end = maximal_intervals[0][0], maximal_intervals[- 1][1]
     trivial_head = [(left_end - trivial_tail_length + i, left_end - trivial_tail_length + i + 1)
                     for i in range(trivial_tail_length)]
@@ -15,28 +17,40 @@ def draw_diagram(word: str, trivial_tail_length=100, range_around_center=5, cent
                     for i in range(trivial_tail_length)]
     intervals, maximal_intervals = [
         trivial_head + arr + trivial_tail for arr in (intervals, maximal_intervals)]
+
     arcs = [dict(type="path",
                  path=semicircle_path(interval[0], interval[1]),
                  line_width=3 if interval in maximal_intervals else 2) for interval in intervals]
     x_ticks = [intervals[0][0]] + [interval[1] for interval in intervals]
     x_center = basepoint if center == 'the basepoint' else 0
     x_range = [x_center - range_around_center, x_center + range_around_center]
+    x_labels = [str(Fraction(number))
+                for number in x_ticks] if format == 'dyadic fraction' else x_ticks
+
     fig = go.Figure()
     fig.update_layout(shapes=arcs)
     fig.update_layout(xaxis={
         'tickmode': 'array',
         'tickvals': x_ticks,
-        'ticktext': x_ticks,
+        'ticktext': x_labels,
         'range': x_range
     },
         yaxis={
         'scaleanchor': 'x',
         'scaleratio': 1,
         'showticklabels': False,
-    },)
+    },
+        legend={
+        'orientation': 'h',
+        'x': 0.0,
+        'y': -0.25,
+        'xanchor': 'center',
+        'yanchor': 'bottom',
+    })
     fig.add_trace(go.Scatter(x=[basepoint], y=[0],
                              mode='markers',
                              marker_size=10,
                              name='basepoint',
+                             hoverinfo='x',
                              showlegend=True))
     return fig
